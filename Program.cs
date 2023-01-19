0a48f97bea6be7a6a267ff93317e781e95e447a1
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-01-19 18:43:46 +0300

    Правильное решение задачи

--- Program.cs
+++ Program.cs
@@ -4,12 +4,18 @@
 Console.Write("Введите цифру соответствующую дню недели: ");
 int X = Convert.ToInt32(Console.ReadLine());
 
-if (X >= 1 && X <= 0)
+if (X >= 1 && X <= 7)
 {
     if (X == 6 || X == 7)
     {
         Console.WriteLine("Да");
     }
+    else
+    {
     Console.WriteLine("Нет");
+    }
 }
-Console.WriteLine("Неверно введён номер дня недели");
+else
+{
+    Console.WriteLine("Неверно введён номер дня недели");
+}
